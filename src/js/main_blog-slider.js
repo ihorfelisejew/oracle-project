efad856e5b6bab226d35ae12f5c5e13e02b8ebf5
Ігор Фелісєєв <lifeofdream2602@gmail.com>
2025-08-04 22:52:38 +0300

    add mobile version

--- src/js/main_blog-slider.js
+++ src/js/main_blog-slider.js
@@ -2,6 +2,12 @@ const swiper = new Swiper(".blog__list", {
 	slidesPerView: 1.1,
 	spaceBetween: 10,
 	breakpoints: {
+		468: {
+			slidesPerView: 1.3,
+		},
+		570: {
+			slidesPerView: 1.7,
+		},
 		768: {
 			slidesPerView: 2.2,
 		},
