259d92ee1bdc4fabec6dff3706775ab0d444f819
Ігор Фелісєєв <lifeofdream2602@gmail.com>
2025-09-02 12:17:20 +0300

    commit final changes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,19 +1,21 @@
-var gulp 			= require('gulp');
-var cssnano 		= require('cssnano');
-var config 			= require('../config');
-var mode        	= require('gulp-mode')();
-var notify 			= require('gulp-notify');
-var browserSync 	= require('browser-sync');
-var autoprefixer 	= require('autoprefixer');
-var postcss			= require('gulp-postcss');
-var cssimport 		= require('gulp-cssimport');
-var sourcemaps 		= require('gulp-sourcemaps');
-var sass 			= require('gulp-sass')(require('sass'));
+var gulp = require("gulp");
+var cssnano = require("cssnano");
+var config = require("../config");
+var mode = require("gulp-mode")();
+var notify = require("gulp-notify");
+var browserSync = require("browser-sync");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var postcss = require("gulp-postcss");
+var cssimport = require("gulp-cssimport");
+var url = require("postcss-url");
+var sourcemaps = require("gulp-sourcemaps");
+var sass = require("gulp-sass")(require("sass"));
 
-var reload      	= browserSync.reload;
+var reload = browserSync.reload;
 
 const sassOptions = {
-	outputStyle: 'compressed',
+	outputStyle: "compressed",
 	sourceMap: true,
 	precision: 3,
 	errLogToConsole: true,
@@ -21,35 +23,46 @@ const sassOptions = {
 
 var processors = [
 	autoprefixer({
-		overrideBrowserslist: [ 'last 15 versions' ],
-		cascade: false
+		overrideBrowserslist: ["last 15 versions"],
+		cascade: false,
 	}),
 	cssnano(),
-]
+	url({
+		url: function (asset) {
+			if (asset.url.indexOf("fonts/") === 0) {
+				return asset.url.replace("fonts/", "");
+			}
+			return asset.url;
+		},
+	}),
+];
 
-gulp.task('styles', (done) => {
-	gulp.src([config.src.sass + '**.scss'])
-	.pipe(mode.development(sourcemaps.init()))
-	.pipe(sass(sassOptions).on('error', (error) => {
-		notify.onError({
-			title: 'Sass Error!',
-			message: error.message
-		})
-	}))
-	.pipe(cssimport())
-	.pipe(mode.production(postcss(processors)))
-	.pipe(mode.development(sourcemaps.write( './' )))
-	.pipe(gulp.dest(config.dest.css))
-	.pipe(gulp.dest(config.theme.assets))
-	.pipe(browserSync.stream())
-	done()
+gulp.task("styles", (done) => {
+	gulp.src([config.src.sass + "**.scss"])
+		.pipe(mode.development(sourcemaps.init()))
+		.pipe(
+			sass(sassOptions).on("error", (error) => {
+				notify.onError({
+					title: "Sass Error!",
+					message: error.message,
+				});
+			})
+		)
+		.pipe(cssimport())
+		.pipe(postcss(processors))
+		.pipe(mode.development(sourcemaps.write("./")))
+		.pipe(gulp.dest(config.dest.css))
+		.pipe(gulp.dest(config.theme.assets))
+		.pipe(browserSync.stream());
+	done();
 });
 
-gulp.task('styles:watch', () => {
-	gulp.watch([
-		config.src.sass + '**/*',
-	], gulp.series('styles', (done) => {
-		reload()
-		done()
-	}))
-})
+gulp.task("styles:watch", () => {
+	gulp.watch(
+		[config.src.sass + "**/*"],
+		gulp.series("styles", (done) => {
+			reload();
+			done();
+		})
+	);
+});
---
+++ src/js/product_info-menu.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const list = document.querySelector(".general-info__list");
+	const menuButtons = document.querySelectorAll(
+		".general-info__menu .menu__item"
+	);
+	const infoItems = document.querySelectorAll(
+		".general-info__list .list__item"
+	);
+
+	menuButtons.forEach((button) => {
+		button.addEventListener("click", () => {
+			if (button.classList.contains("active")) return;
+
+			menuButtons.forEach((insideButton) => {
+				insideButton.classList.remove("active");
+			});
+
+			button.classList.add("active");
+
+			infoItems.forEach((item) => {
+				const dataInfoBlock = button.dataset.item;
+				if (item.classList.contains(dataInfoBlock)) {
+					item.classList.add("active");
+				} else {
+					item.classList.remove("active");
+				}
+			});
+		});
+	});
+});
